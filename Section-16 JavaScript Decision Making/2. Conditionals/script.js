003859d44d9c6d3755c732ec138585b6c422879f
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-09-13 07:27:10 +0530

    Add files via upload

---
+++ Section-16 JavaScript Decision Making/2. Conditionals/script.js
@@ -0,0 +1,38 @@
+// IF
+
+// console.log("Before Conditionals");
+// if(1 + 2 === 2){
+//     console.log("Math Still Works")
+// }
+// let random = Math.random();
+// if(random < 0.5){
+//    console.log("Your Number is less than 0.5")
+//    console.log(random)
+// }
+// if(random > 0.5){
+//    console.log("Your Number is greater than 0.5")
+//    console.log(random)
+// }
+// console.log("After Conditionals");
+
+// ELSE IF
+
+// const dayOfWeek = "Friday";
+
+// if (dayOfWeek === "Monday") {
+//     console.log("UGHH I HATE MONDAYS!!!!");
+// } else if(dayOfWeek === "Saturday") {
+//     console.log("Yay I love Saturdays!!!");
+// } else if(dayOfWeek === "Friday") {
+//     console.log("Fridays are Decent, Especially After Work!!!");
+// }
+
+const age = 30;
+
+if(age < 5) {
+    console.log("You are a baby. You get in for free!!");
+} else if(age < 10){
+    console.log("You are child. You pay $10");
+} else if(age < 65) {
+    console.log("Your are an adult. You pay $20");
+} 
