69ccafd791e2812453f2981716ae0dc704656f04
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-06 09:24:38 +0530

    Add files via upload

---
+++ Section-19 Repeating Stuffs With Loops/For Loops/script.js
@@ -0,0 +1,20 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// for(let i=1; i<=10; i+=1) {
+//     console.log("I'M IN THE LOOP BODY!!!!!");
+//     console.log(i);
+// }
+
+for(let i = 0; i <= 20; i=2)
+{
+    console.log(i);
+}
