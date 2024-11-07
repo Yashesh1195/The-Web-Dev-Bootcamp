a493661f74bb9fce4b4eb42f9a42e699af8c4671
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-07 08:57:36 +0530

    Add files via upload

---
+++ Section-19 Repeating Stuffs With Loops/While Loops/script.js
@@ -0,0 +1,15 @@
+// let count = 0;
+// while(count<10)
+// {
+//     count++;
+//     console.log(count);
+// }
+
+// const SECRET = "BabyHippo";
+
+// let guess = prompt("Enter the Secret Code...");
+// while(guess !== SECRET)
+// {
+//     guess = prompt("Enter the Secret Code...");
+// }
+// console.log("CONGRATS!!! YOU GOT THE SECRET!!!");
