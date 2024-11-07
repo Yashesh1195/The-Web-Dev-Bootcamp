1c75395784dd85f751a29de265abed7a4cfac3bb
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-07 14:41:39 +0530

    Add files via upload

---
+++ Section-19 Repeating Stuffs With Loops/ForOf Loop/script.js
@@ -0,0 +1,38 @@
+// const subreddits = ['cringe', 'books', 'chickens', 'funny'];
+
+// for(let i = 0; i<subreddits.length; i++)
+// {
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`);
+// }
+
+// for(let sub of subreddits){
+//     console.log(`Visit reddit.in/r/${sub}`);
+// }
+
+// const seatingChart = [
+//     ['ABC', 'DEF', 'GHI'],
+//     ['JKL', 'MNO', 'PQR'],
+//     ['STU', 'VWX', 'YZ']
+// ];
+
+// for(let i = 0; i<=seatingChart.length; i++)
+// {
+//     const row = seatingChart[i];
+//     console.log(`ROW #${i}`);
+//     for(let j = 0; j<row.length; j++)
+//     {
+//         console.log(row[j]);
+//     }
+// }
+
+// for(let row of seatingChart)
+// {
+//     for(let student of row)
+//     {
+//         console.log(student);
+//     }
+// }
+
+// for(let char of "HELLO WORLD!!"){
+//     console.log(char);
+// }
