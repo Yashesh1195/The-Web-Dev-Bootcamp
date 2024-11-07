4a7c2cee32d3259459525a42f527952d421793b6
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-07 15:39:06 +0530

    Add files via upload

--- Section-19 Repeating Stuffs With Loops/Project- To Do List/script.js
+++ Section-19 Repeating Stuffs With Loops/Project- To Do List/script.js
@@ -25,7 +25,15 @@ while(command !== "quit" && command !== "q"){
     //delete command
     else if(command === "delete"){
         let deleteTodo = parseInt(prompt("Enter the index of the Todo to be removed!!"));
-        todos.splice(deleteTodo, 1);
+        //condition to check whether the entered index is proper or not
+       if(!Number.isNaN(deleteTodo))
+       {
+            let deleted = todos.splice(deleteTodo, 1);
+            console.log(`${deleted} is deleted!!!!!`);
+       }
+       else{
+        console.log("Unknown Index Entered!!");
+       }
     }
 
     command = prompt("What would you like to do?");
