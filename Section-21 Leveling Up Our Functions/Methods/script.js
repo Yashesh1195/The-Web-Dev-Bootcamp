a5ef4e7e1145ee714b0a6e21f5bbf920b3c1e013
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-10 17:32:02 +0530

    Add files via upload

--- Section-21 Leveling Up Our Functions/Methods/script.js
+++ Section-21 Leveling Up Our Functions/Methods/script.js
@@ -10,15 +10,38 @@
 //     }
 // }
 
-const myMath = {
-    PI: 3.14159,
-    square(num){
-        // return num*num;
-        return Math.pow(num, 2);
-    },
-    cube(num){
-        // return num ** 3;
-        return Math.pow(num, 3);
+// const myMath = {
+//     PI: 3.14159,
+//     square(num){
+//         // return num*num;
+//         return Math.pow(num, 2);
+//     },
+//     cube(num){
+//         // return num ** 3;
+//         return Math.pow(num, 3);
+//     }
+// }
+
+// ============
+// THIS KEYWORD
+// ============
+
+// const person = {
+//     first: "Robert",
+//     last: "Herjavec",
+//     fullName(){
+//         return `${this.first} ${this.last}`
+//     }
+// }
+
+const cat = {
+    name: "Blue Steele",
+    color: "Grey",
+    breed: "Scottish Fold",
+    meow(){
+        console.log("THIS IS:", this);
+        console.log(`${this.name} says MEOWWWW!!!!`);
     }
 }
 
+const meow2 = cat.meow;
