ba11ac6af31599b0d7c959e728de7680f2dbf560
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-10 17:16:33 +0530

    Add files via upload

---
+++ Section-21 Leveling Up Our Functions/Methods/script.js
@@ -0,0 +1,24 @@
+// const math = {
+//     multiply : function(x, y){
+//         return x * y;
+//     },
+//     divide : function(x, y){
+//         return x / y;
+//     },
+//     square : function(x){
+//         return Math.pow(x, 2);
+//     }
+// }
+
+const myMath = {
+    PI: 3.14159,
+    square(num){
+        // return num*num;
+        return Math.pow(num, 2);
+    },
+    cube(num){
+        // return num ** 3;
+        return Math.pow(num, 3);
+    }
+}
+
