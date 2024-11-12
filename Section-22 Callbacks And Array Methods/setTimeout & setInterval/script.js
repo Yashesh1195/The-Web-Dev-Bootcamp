71a0e6e70ea28af60aed99f7c9d8d65c2dd121bb
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-12 09:27:08 +0530

    Add files via upload

---
+++ Section-22 Callbacks And Array Methods/setTimeout & setInterval/script.js
@@ -0,0 +1,17 @@
+//Set Timeout
+
+// console.log("HELLO!!");
+// setTimeout(() => {
+//     console.log("... are you still there?")
+// }, 3000);
+// console.log("GOODBYE!!!");
+
+
+//Set Interval
+
+const id = setInterval(() => {
+    console.log(Math.random())
+}, 2000);
+
+//it will help in stopping the continuous iteration of above setInterval function
+// clearInterval(id);
