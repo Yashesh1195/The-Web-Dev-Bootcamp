18fccf7bf2918aa6f671af565993b648b0aa7b08
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-12 08:52:23 +0530

    Add files via upload

---
+++ Section-22 Callbacks And Array Methods/Arrow Functions/script.js
@@ -0,0 +1,15 @@
+const add = function (x, y){
+    return x+y;
+}
+
+const sum = (x, y) => {
+    return x + y;
+}
+
+const square = num => {
+    return num * num;
+}
+
+const rollDie = () => {
+    return Math.floor(Math.random()*6)+1;
+}
