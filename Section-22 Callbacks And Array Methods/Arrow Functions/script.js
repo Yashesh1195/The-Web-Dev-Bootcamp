9cafc69bafcb800cee52b7a911c6062f71489ae7
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-12 08:53:13 +0530

    Add files via upload

--- Section-22 Callbacks And Array Methods/Arrow Functions/script.js
+++ Section-22 Callbacks And Array Methods/Arrow Functions/script.js
@@ -1,6 +1,6 @@
-const add = function (x, y){
-    return x+y;
-}
+// const add = function (x, y){
+//     return x+y;
+// }
 
 const sum = (x, y) => {
     return x + y;
@@ -10,6 +10,45 @@ const square = num => {
     return num * num;
 }
 
-const rollDie = () => {
-    return Math.floor(Math.random()*6)+1;
-}
+// const rollDie = () => {
+//     return Math.floor(Math.random()*6)+1;
+// }
+
+
+// ===============
+// IMPLICIT RETURN
+// ===============
+
+
+const rollDie = () => (
+    
+    Math.floor(Math.random()*6)+1
+)
+
+const add = (a, b) => a + b
+
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+
+// const newMovies = movies.map(function(movie){
+//     return `${movie.title} - ${movie.score / 10}`
+// })
+
+const newMovies = movies.map((movie) => (`${movie.title} - ${movie.score / 10}`))
