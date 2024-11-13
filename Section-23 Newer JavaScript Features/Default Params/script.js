8b90bd9ffa73306580c5a5bb13a72ef877187e9f
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-13 07:48:09 +0530

    Add files via upload

---
+++ Section-23 Newer JavaScript Features/Default Params/script.js
@@ -0,0 +1,26 @@
+// ==========================================
+// AN OLDER WAY OF ADDING DEFAULT PARAM VALUE
+// ==========================================
+
+// function rollDie(numSides){
+//     if(numSides === undefined){
+//         numSides = 6;
+//     }
+//     return Math.floor(Math.random()*numSides) + 1;
+// }
+
+// ============
+// THE NEW WAY!
+// ============
+
+function rollDie(numSides = 6){
+    return Math.floor(Math.random()*numSides) + 1;
+}
+
+function product(a, b=1){
+    return a*b;
+}
+
+function greet(person, msg="Hey There", punc = "!"){
+    console.log(`${msg}, ${person}${punc}`);
+}
