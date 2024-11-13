e482f98d4f31644da896a81b885cc71845f32dee
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-13 08:19:32 +0530

    Add files via upload

--- Section-23 Newer JavaScript Features/Spread/script.js
+++ Section-23 Newer JavaScript Features/Spread/script.js
@@ -14,3 +14,17 @@ const dogs = ["Rusty", "Wyatt"];
 const allPets = [...cats, ...dogs]
 
 // SPREAD IN OBJECTS
+
+const feline = { legs: 4, family: "Felidae"};
+const canine = { isFurry: true, family: "Caninae"};
+
+const catDog = { ...feline, ...canine };
+
+
+const dataFromForm = {
+    email: "blueman@gmail.com",
+    password: "tobias123!",
+    username: "tfunkie"
+}
+
+const newUser = {...dataFromForm, id: 2345, isAdmin: false};
