ed0ab28bc47e06675bacbe9457a43e574e9331f3
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-13 08:07:13 +0530

    Add files via upload

--- Section-23 Newer JavaScript Features/Spread/script.js
+++ Section-23 Newer JavaScript Features/Spread/script.js
@@ -1,2 +1,16 @@
 // The JavaScript spread operator (...) allows us to quickly copy all or part of an existing array or object into another array or object.
 
+// SPREAD IN FUNCTIONS
+const nums = [13, 4, 5, 21, 3, 3, 1, 2, 7, 6, 4, 2, 53456];
+// let max = Math.max(nums) //NaN
+// let max = Math.max(...nums) //53456
+// console.log(..."hello");
+
+// SPREAD IN ARRAYS
+
+const cats = ["Blue", "Scout", "Rocket"];
+const dogs = ["Rusty", "Wyatt"];
+
+const allPets = [...cats, ...dogs]
+
+// SPREAD IN OBJECTS
