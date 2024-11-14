9693b9ee79d888be21cab733c2cb70873a66a43d
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2024-11-14 14:00:03 +0530

    Add files via upload

---
+++ Section-24 Introducing The World Of The DOM/Selecting/script.js
@@ -0,0 +1,32 @@
+//===========================
+// document.getElementById();
+//===========================
+
+// const banner = document.getElementById("banner");
+// const toc = document.getElementById("toc");
+
+//=================================
+// document.getElementsByTagName();
+//=================================
+
+// const allImages = document.getElementsByTagName('img');
+
+// for(let img of allImages){
+//     // console.log(img.src);
+//     img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg"
+// }
+
+//===================================
+// document.getElementsByClassName();
+//===================================
+
+// const squareImages = document.getElementsByClassName('square');
+
+// for(let img of squareImages){
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+// }
+
+//=============================
+// document.querySelectorAll();
+//=============================
+
