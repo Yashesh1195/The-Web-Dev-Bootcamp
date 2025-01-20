f09a9d0dc2f1fe55a3839660c0b129ca4e5503b5
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-01-20 08:08:56 +0530

    Add files via upload

--- Section-25 The Missing Piece DOM Events/Event Basics/script.js
+++ Section-25 The Missing Piece DOM Events/Event Basics/script.js
@@ -19,3 +19,29 @@ btn.onmouseenter = scream;
 document.querySelector('h1').onclick = () => {
     alert("You Clicked H1!!!!!!");
 }
+
+// addEventListener
+
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener('dblclick', function() {
+    alert("CLICKED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+});
+btn3.addEventListener('dblclick', scream);
+
+function twist(){
+    console.log("TWIST");
+}
+
+function shout(){
+    console.log("SHOUT");
+}
+
+const tasButton = document.querySelector("#tas");
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+//It will over write shout over twist
+
+tasButton.addEventListener('click', twist, {once: true});
+tasButton.addEventListener('click', shout, {once: true});
+//th problem of overwriting is handled by this function 
