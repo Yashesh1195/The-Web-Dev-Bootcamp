6a6e1531de486bd18507b6aac10625d4caba6b21
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-01-21 07:18:27 +0530

    Add files via upload

---
+++ Section-25 The Missing Piece DOM Events/Random Colors/script.js
@@ -0,0 +1,22 @@
+const button = document.querySelector('button');
+const h1 = document.querySelector('#heading');
+
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    if(r<=100 && g<=100 && b<=100){
+        h1.style.color="white";
+    }
+    else{
+        h1.style.color="black";
+    }
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+button.addEventListener('click', function() {
+    const newColor = randomColor();
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+   
+});
