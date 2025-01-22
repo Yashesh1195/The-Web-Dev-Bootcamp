bbf15e0dced987a93d18a6f585a90b1c108db077
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-01-22 07:46:40 +0530

    Add files via upload

--- Section-25 The Missing Piece DOM Events/Form Events (Old)/script.js
+++ Section-25 The Missing Piece DOM Events/Form Events (Old)/script.js
@@ -1,1 +1,25 @@
+const tweetForm = document.querySelector("#tweetForm");
+const tweetsContainer = document.querySelector("#tweets");
 
+tweetForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    // console.log("SUBMIT!!!");
+    // const usernameInput = document.querySelectorAll("input")[0];
+    // const tweetInput = document.querySelectorAll("input")[1];
+    const userName = tweetForm.elements.username;
+    const tweet = tweetForm.elements.tweet;
+    addTweet(userName.value, tweet.value);
+    userName.value="";
+    tweet.value="";
+    
+    // console.log(usernameInput.value, tweetInput.value);
+});
+
+const addTweet = (userName, tweet) => {
+    const newTweet = document.createElement("li");
+    const bTag = document.createElement("b");
+    bTag.append(userName);
+    newTweet.append(bTag);
+    newTweet.append(`- ${tweet}`);
+    tweetsContainer.append(newTweet);
+}
