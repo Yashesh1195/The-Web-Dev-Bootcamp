c9465ef9adaeee958ee705c3323262cffff71046
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-01-22 07:14:51 +0530

    Add files via upload

--- Section-25 The Missing Piece DOM Events/Form Events/script.js
+++ Section-25 The Missing Piece DOM Events/Form Events/script.js
@@ -1,7 +1,16 @@
 // The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
 
 const form = document.querySelector("#shelterForm");
+const input  = document.querySelector("#catName");
+const list = document.querySelector("#cats");
 form.addEventListener("submit", function(e){
     e.preventDefault();
-    console.log("SUBMITTED!!!!!!!");
+    // console.log(input.value);
+    const catName = input.value;
+    const newLI = document.createElement("LI");
+    newLI.innerText = catName;
+    // console.log(newLI);
+    list.append(newLI);
+    //used to empty the form data previusly updated 
+    input.value = "";
 });
