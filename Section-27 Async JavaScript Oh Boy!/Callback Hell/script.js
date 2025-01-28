d52cd3efc9e974cb128f97ae5c4c669da703f6e0
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-01-28 07:59:31 +0530

    Add files via upload

---
+++ Section-27 Async JavaScript Oh Boy!/Callback Hell/script.js
@@ -0,0 +1,45 @@
+// setTimeout(() => {
+//     document.body.style.backgroundColor = "red";
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = "orange";
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = "lightblue";
+//             setTimeout(() => {
+//                 document.body.style.backgroundColor = "darkblue";
+//                 document.body.style.color = "white";
+//                 setTimeout(() => {
+//                     document.body.style.backgroundColor = "green";
+//                 }, 1000)
+//             }, 1000)
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext();
+    }, delay)
+}
+delayedColorChange("olive", 1000, () => {
+    delayedColorChange("orange", 1000, () => {
+        delayedColorChange("magenta", 1000, () => {
+            delayedColorChange("lightblue", 1000, () => {
+                delayedColorChange("red", 1000, () => {
+        
+                })
+            })
+        })
+    })
+})
+
+
+searchMoviesAPI('amadeus', () => {
+    saveToMyDB(movies, () => {
+        //If it works, run this
+    }, () => {
+        //If it doesn't work, run this
+    })
+}, () => {
+    //If API id down, or request failed
+})
