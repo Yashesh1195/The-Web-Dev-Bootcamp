c5dc7e424fff8c1f74cf048833e52fd11a962a55
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-01-29 07:28:47 +0530

    Add files via upload

--- Section-27 Async JavaScript Oh Boy!/Promises Intro/script.js
+++ Section-27 Async JavaScript Oh Boy!/Promises Intro/script.js
@@ -51,24 +51,44 @@ const fakeRequestPromise = (url) => {
 //     console.log(error);
 // })
 
+// fakeRequestPromise('yelp.com/api/coffee/page1')
+//     .then(() => {
+//         console.log("IT WORKED!!!!!!! (page1)");
+//         fakeRequestPromise('yelp.com/api/coffee/page2')
+//             .then(() => {
+//                 console.log("IT WORKED!!!!!!! (page2)");
+//                 fakeRequestPromise('yelp.com/api/coffee/page3')
+//                     .then(() => {
+//                         console.log("IT WORKED!!!!!!! (page3)");
+//                     })
+//                     .catch(() => {
+//                         console.log("OH NO, ERROR!!!! (page3)");
+//                     })
+//             })
+//             .catch(() => {
+//                 console.log("OH NO, ERROR!!!! (page2)");
+//             })
+//     })
+//     .catch(() => {
+//         console.log("OH NO, ERROR!!!! (page1)");
+//     })
+
 fakeRequestPromise('yelp.com/api/coffee/page1')
-    .then(() => {
+    .then((data) => {
         console.log("IT WORKED!!!!!!! (page1)");
-        fakeRequestPromise('yelp.com/api/coffee/page2')
-            .then(() => {
-                console.log("IT WORKED!!!!!!! (page2)");
-                fakeRequestPromise('yelp.com/api/coffee/page3')
-                    .then(() => {
-                        console.log("IT WORKED!!!!!!! (page3)");
-                    })
-                    .catch(() => {
-                        console.log("OH NO, ERROR!!!! (page3)");
-                    })
-            })
-            .catch(() => {
-                console.log("OH NO, ERROR!!!! (page2)");
-            })
+        console.log(data)
+        return fakeRequestPromise('yelp.com/api/coffee/page2')
     })
-    .catch(() => {
-        console.log("OH NO, ERROR!!!! (page1)");
+    .then((data) => {
+        console.log("IT WORKED!!!!!!! (page2)");
+        console.log(data);
+        return fakeRequestPromise('yelp.com/api/coffee/page3')
+    })
+    .then((data) => {
+        console.log("IT WORKED!!!!!!! (page3)");
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log("OH NO, REQUEST FAILED!!");
+        console.log(error);
     })
