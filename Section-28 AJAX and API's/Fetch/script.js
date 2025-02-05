b0347b63d65758d810b9f0258a1b73e89d2e457d
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-02-05 07:15:54 +0530

    Add files via upload

---
+++ Section-28 AJAX and API's/Fetch/script.js
@@ -0,0 +1,48 @@
+// fetch("https://swapi.tech/api/people/1/")
+// .then((res) => {
+//     console.log("RESOLVED", res);
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log(data.result.properties);
+// })
+// .catch((e) => {
+//     console.log("ERROR!!", e);
+// })
+
+// fetch("https://swapi.tech/api/people/1/")
+// .then((res) => {
+//     console.log("RESOLVED", res);
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log(data.result.properties);
+//     return fetch("https://swapi.tech/api/people/1/"); 
+// })
+// .then((res) => {
+//     console.log("Second Request Resolved");
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log(data.result.properties);
+//     return fetch("https://swapi.tech/api/people/1/"); 
+// })
+// .catch((e) => {
+//     console.log("ERROR!!", e);
+// })
+
+const loadStarWarsPeople = async () => {
+    try {
+        const res = await fetch("https://swapi.tech/api/people/1/");
+        const data = await res.json();
+        console.log(data.result.properties);
+        const res2 = await fetch("https://swapi.tech/api/people/2/");
+        const data2 = await res2.json();
+        console.log(data2.result.properties);
+    }
+    catch(e) {
+        console.log("ERROR!!!", e);
+    }
+}
+
+loadStarWarsPeople();
