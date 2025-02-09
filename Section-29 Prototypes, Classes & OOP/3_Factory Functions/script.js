769e8423b19013540830eea80f8dbb75723b334f
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-02-09 09:50:23 +0530

    Add files via upload

---
+++ Section-29 Prototypes, Classes & OOP/3_Factory Functions/script.js
@@ -0,0 +1,11 @@
+function hex(r, g, b) {
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function rgb(r, g, b) {
+    return `rgb(${r}, ${b}, ${g})`;
+}
+
+hex(255, 100, 25);
+// "#ff6419"
+// "rgb(255, 100, 25)"
