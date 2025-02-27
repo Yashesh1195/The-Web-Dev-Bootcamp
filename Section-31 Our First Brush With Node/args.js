32a64877379fa251358cadc22297d1bb27d5f180
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-02-27 22:27:30 +0530

    Add files via upload

---
+++ Section-31 Our First Brush With Node/args.js
@@ -0,0 +1,3 @@
+// The process.argv property returns an array containing the command-line arguments passed when the Node.js process was launched. The first element will be process.execPath. See process.argv0 if access to the original value of argv[0] is needed. The second element will be the path to the JavaScript file being executed. The remaining elements will be any additional command-line arguments.
+console.log("HELLO FROM ARGS FILE!!!");
+console.log(process.argv);
