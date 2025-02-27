445505986d8f97a5943f02c696fc43e6539f0a06
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-02-27 22:53:37 +0530

    Add files via upload

---
+++ Section-31 Our First Brush With Node/boilerplate.js
@@ -0,0 +1,24 @@
+// All file system operations have synchronous, callback, and promise-based forms, and are accessible using both CommonJS syntax and ES6 Modules (ESM).
+
+const fs = require('fs');
+const folderName = process.argv[2] || "Project";
+// console.log(fs);
+
+//Async Version of mkdir
+// fs.mkdir('Project1/ProjectFiles', { recursive: true }, (err) => {
+//     console.log("IN THE CALLBACK!!");
+//     if (err) throw err;
+// }); 
+
+//Sync version of mkdir
+try{
+    fs.mkdirSync(folderName);
+fs.writeFileSync(`${folderName}/index.html`, ' ');
+fs.writeFileSync(`${folderName}/style.css`, ' ');
+fs.writeFileSync(`${folderName}/script.js`, ' ');
+}
+catch(e) {
+    console.log("Somehting went wrong!!");
+    console.log(e);
+}
+console.log("I COME AFTER MKDIR IN THE FILE!!!");
