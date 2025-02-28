00e0e099670b567f9cd92845f3c1dddc19f3468f
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-02-28 08:27:39 +0530

    Add files via upload

--- Section-32 Exploring Modules and the NPM Universe/script.js
+++ Section-32 Exploring Modules and the NPM Universe/script.js
@@ -1,4 +1,8 @@
-const math = require('./math');
-console.log(math.PI);
-console.log(math.square(11));
-console.log(math.add(5, 22));
+// const math = require('./math');
+// console.log(math.PI);
+// console.log(math.square(11));
+// console.log(math.add(5, 22));
+
+const cats = require('./shelter');
+
+console.log("REQUIRED AN ENTIRE DIRECTORY!!", cats);
