0107ef0b5d093c9bdf7f56fda331aa0a1ef7ef39
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-03-01 23:28:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// const math = require('./math');
+// console.log(math.PI);
+// console.log(math.square(161));
+// console.log(math.add(51, 22));
+
+//===OR===
+
+// const { PI, square } = require('./math');
+// console.log(PI);
+// console.log(square(1119));
+
+const cats = require('./shelter');
+
+console.log("REQUIRED AN ENTIRE DIRECTORY!!", cats);
