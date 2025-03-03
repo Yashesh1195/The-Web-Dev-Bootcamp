e8312a0621862c7cd27ae0d78e404925195ed6f0
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-03-03 08:34:09 +0530

    Add files via upload

--- Section-33 Creating Servers With Express/index.js
+++ Section-33 Creating Servers With Express/index.js
@@ -2,9 +2,32 @@ const express = require('express');
 const app = express();
 // console.dir(app);
 
-app.use(() => {
-    console.log("WE GOT A NEW REQUEST!");
-});
+// app.use((req, res) => {
+//     console.log("WE GOT A NEW REQUEST!");
+//     // console.dir(req);
+//     // res.send("HELLO, WE GOT YOUR REQUEST! THIS IS A RESPONSE");
+//     // res.send({color: 'red'});
+//     res.send('<h1>This is my webpage!</h1>');
+// });
+
+// /cats = 'meow'
+// /dogs = 'woof'
+// / = 'Welcome To Our Home Page!'
+
+app.get('/cats', (req, res) => {
+    // console.log("CAT REQUEST!!!");
+    res.send("MEOW!!!");
+})
+
+app.get('/dogs', (req, res) => {
+    // console.log("DOG REQUEST!!!");
+    res.send("WOOFF!!!");
+})
+
+app.get('/', (req, res) => {
+    res.send("This is the Home Page!!!");
+})
+
 
 app.listen(8080, () => {
     console.log("LISTENING ON PORT 8080!");
