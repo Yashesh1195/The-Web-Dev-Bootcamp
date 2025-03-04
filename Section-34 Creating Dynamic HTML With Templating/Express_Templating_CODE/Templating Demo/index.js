668fcd519d48e5a93c6304e508f174b8b2332ca2
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-03-04 08:25:38 +0530

    Add files via upload

--- Section-34 Creating Dynamic HTML With Templating/Express_Templating_CODE/Templating Demo/index.js
+++ Section-34 Creating Dynamic HTML With Templating/Express_Templating_CODE/Templating Demo/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Assigns setting name to value. You may store any value that you want, but certain names can be used to configure the behavior of the server. 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
     // res.send("HI!!!!");
-    res.render('home.ejs');
+    // res.render('home.ejs');
+    res.render('home');
 })
 
 app.listen(3000, () => {
