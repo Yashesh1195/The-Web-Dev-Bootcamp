b2589f0cf1926018ad02264537e31c8a6a0afe4b
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-03-05 21:56:01 +0530

    Add files via upload

--- Section-34 Creating Dynamic HTML With Templating/Express_Templating_CODE/index.js
+++ Section-34 Creating Dynamic HTML With Templating/Express_Templating_CODE/index.js
@@ -1,12 +1,49 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const redditData = require('./data.json');
+
+// To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Assigns setting name to value. You may store any value that you want, but certain names can be used to configure the behavior of the server. 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
     // res.send("HI!!!!");
-    res.render('home.ejs');
+    // res.render('home.ejs');
+    res.render('home');
+})
+
+//Loops in EJS
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+    ]
+    // res.render('cats', { allCats: cats })
+    res.render('cats', { cats })
+})
+
+//Subreddit Template Demo
+app.get('/r/:subreddit', (req, res) => {
+    const{ subreddit } = req.params;
+    const data = redditData[subreddit];
+    console.log(data);
+    if(data) {
+        res.render('subreddit', { ...data });
+    }
+    else {
+        res.render('notfound', { subreddit });
+    }
+})
+
+//Passing Data To Templates
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    // res.render('random', {rand: num});
+    // res.render('random', {num: num});
+    res.render('random', { num });
 })
 
 app.listen(3000, () => {
