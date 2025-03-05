07c03c81836450728682beb28ccfe74cc16b3ab9
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-03-05 07:46:37 +0530

    Add files via upload

--- Section-34 Creating Dynamic HTML With Templating/Express_Templating_CODE/Templating_Demo/index.js
+++ Section-34 Creating Dynamic HTML With Templating/Express_Templating_CODE/Templating_Demo/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Assigns setting name to value. You may store any value that you want, but certain names can be used to configure the behavior of the server. 
 app.set('view engine', 'ejs');
@@ -12,10 +15,26 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+//Loops in EJS
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+    ]
+    // res.render('cats', { allCats: cats })
+    res.render('cats', { cats })
+})
+
 //Subreddit Template Demo
 app.get('/r/:subreddit', (req, res) => {
     const{ subreddit } = req.params;
-    res.render('subreddit', { subreddit });
+    const data = redditData[subreddit];
+    console.log(data);
+    if(data) {
+        res.render('subreddit', { ...data });
+    }
+    else {
+        res.render('notfound', { subreddit });
+    }
 })
 
 //Passing Data To Templates
