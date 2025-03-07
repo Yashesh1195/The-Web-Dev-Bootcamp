b352544386ffc93aa1b4dddde6a1d32d84427a81
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-03-07 07:16:34 +0530

    Add files via upload

--- Section-35 Defining RESTful Routes/RESTDemo/index.js
+++ Section-35 Defining RESTful Routes/RESTDemo/index.js
@@ -1,19 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get('/tacos', (req, res) => {
-    res.send("GET /tacos response");
-})
+const comments = [
+  {
+    username: "todd",
+    comment: "lol that is so funny",
+  },
+  {
+    username: "Skyler",
+    comment: "I like to go birdwatching with my dog",
+  },
+  {
+    username: "Sk8erBoi",
+    comment: "Plz delete your account, Todd",
+  },
+  {
+    username: "onlysayswoof",
+    comment: "woof woof woof",
+  },
+];
 
-app.post('/tacos', (req, res) => {
-    // res.send("POST /tacos response");
-    const { meat, qty } = req.body;
-    res.send(`OK, Here are your ${qty} ${meat} tacos`);
-})
+app.get("/tacos", (req, res) => {
+  res.send("GET /tacos response");
+});
+
+app.post("/tacos", (req, res) => {
+  // res.send("POST /tacos response");
+  const { meat, qty } = req.body;
+  res.send(`OK, Here are your ${qty} ${meat} tacos`);
+});
 
 app.listen(3000, () => {
-    console.log("ON PORT 3000!");
-})
+  console.log("ON PORT 3000!");
+});
+
+// username
+// express.text
+
+// bob - hello!
+
+// GET /allcomments
+// GET /all
+// GET /showmeallcomments
+
+// POST /newcomment
+// POST /makecomment
+
+// GET /comments = list all comments
+// POST /comments = Create a new comment
+// GET /comments/:id = GET one comment
+// PATCH /comments/:id = Update on comment
+// DELETE /comments/:id = Destroy on comment
