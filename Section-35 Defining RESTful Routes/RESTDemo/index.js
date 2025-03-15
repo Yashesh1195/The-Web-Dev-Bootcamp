80b9afd774458cfef990bf0bf56445433bb23b72
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-03-15 15:17:00 +0530

    Add files via upload

--- Section-35 Defining RESTful Routes/RESTDemo/index.js
+++ Section-35 Defining RESTful Routes/RESTDemo/index.js
@@ -1,33 +1,36 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
+const methodOverride = require("method-override");
 
 // It is a built-in middleware function in Express. It parses incoming requests with urlencoded payloads and it is based on body parser. 
 app.use(express.urlencoded({ extended: true }));
 // It is a built-in middleware function in Express. It parses incoming requests with JSON payloads and it is based on body parser. 
 app.use(express.json());
+app.use(methodOverride('_method'))
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const comments = [
   {
-    id: 1,
+    id: uuid(),
     username: "Todd",
     comment: "lol that is so funny",
   },
   {
-    id: 2,
+    id: uuid(),
     username: "Skyler",
     comment: "I like to go birdwatching with my dog",
   },
   {
-    id: 3,
+    id: uuid(),
     username: "Sk8erBoi",
     comment: "Plz delete your account, Todd",
   },
   {
-    id: 4,
+    id: uuid(),
     username: "onlysayswoof",
     comment: "woof woof woof",
   },
@@ -47,24 +50,41 @@ app.get('/comments/new', (req, res) => {
 app.post('/comments', (req, res) => {
   // console.log(req.body);
   const { username, comment } = req.body;
-  comments.push({username, comment});
+  comments.push({username, comment, id: uuid()});
   // res.send("IT WORKED!!!!");
   res.redirect('/comments');
 })
 
-//Show /comments/:id :- Get Details for one specific comment
+//Show /comments/:id :- GET- Details for one specific comment
 app.get('/comments/:id', (req, res) => {
   const { id } = req.params;
-  const comment = comments.find(c => c.id === parseInt(id));
+  const comment = comments.find(c => c.id === id);
   res.render('comments/show', { comment })
 
 })
 
-//Edit /comments/:id/edit :- GET Form to edit specific comment
+//Edit /comments/:id/edit :- GET- Form to edit specific comment
+app.get('/comments/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find(c => c.id === id);
+  res.render('comments/edit', { comment });
+})
 
-//Update /comments/:id :- PATCH Updates specific comment on server
+//Update /comments/:id :- PATCH- Updates specific comment on server
+app.patch('/comments/:id', (req, res) => {
+  // res.send("UPDATING!!!!");
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find(c => c.id === id);
+  foundComment.comment = newCommentText;
+  res.redirect("/comments");
+})
 
-//Destroy /comments/:id :- DELETE Deletes specific item on server
+//Destroy /comments/:id :- DELETE- Deletes specific item on server
+app.delete('/comments/:id', (req, res) => {
+  
+})
+
 
 // app.get("/tacos", (req, res) => {
 //   res.send("GET /tacos response");
